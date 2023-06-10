69ecca9ae6d62582d174ad28601d3e34c153d065
Nilkant Gunjote <nilkantgunjote.com>
2023-06-10 16:32:27 +0530

    Fixed Redirect

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-// import { redirect } from 'next/navigation';
-// export default async function Home({ params }) {
-//   redirect('/');
+"use client";
+import { useEffect } from "react";
 
-// }
+const SuccessPage = () => {
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      console.log("called");
+      window.location.href = "/login";
+    }, 2000);
 
-const page = () => {
+    return () => {
+      clearTimeout(redirectTimeout);
+    };
+  }, []);
+
   return (
     <div className="page__wrapper">
-      <div className="content__div relative success__wrapper">
+      <div className="content_div relative success_wrapper">
         {/* Blobs */}
         <div className="blob1 absolute"></div>
         <div className="blob2 absolute"></div>
@@ -29,14 +36,16 @@ const page = () => {
         <div className="blob4 absolute"></div>
         <div className="blob4 absolute"></div>
         <div className="blob4 absolute"></div>
+        <div className="blob4 absolute"></div>
+        <div className="blob4 absolute"></div>
 
         {/* Main code */}
-       <div className="success__div">
-       <h4 className="green__text">Successful</h4>
-       </div>
+        <div className="success__div">
+          <h4>Successful</h4>
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default SuccessPage;
